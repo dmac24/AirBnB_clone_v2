8107afb12d46532e7914c80fa9f57cc39d9cc962
PabloOsorix <3387@holbertonschool.com>
2022-05-14 18:39:27 -0500

    :heavy_plus_sign: add name

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
+    """atributtes to create a State Table
+    in the database"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)    
     """ State class """
     name = ""
