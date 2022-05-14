086cbc1b648244711f1e2dbb0fe5311328a17031
PabloOsorix <3387@holbertonschool.com>
2022-05-14 10:26:11 -0500

    :heavy_plus_sign: script that create database and user for tests

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Script that create database and user for test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
