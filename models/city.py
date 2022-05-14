2f876a8094de734f157dbdd1a43f50a1fc723cb4
PabloOsorix <3387@holbertonschool.com>
2022-05-14 18:36:59 -0500

    :heavy_plus_sign: add class atributte name, state_id, states_id

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
+    """Atributtes to create a class table city"""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states_id'),
+                      nullable=False)
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
