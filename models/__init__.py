d196ab96966670be9ec540e986925100643d4dd4
PabloOsorix <3387@holbertonschool.com>
2022-05-14 18:41:55 -0500

    :heavy_plus_sign: add conditional to create an instance DBSstorage or FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object
+of class DBStorage or FileStorage
+"""
+from os import getenv
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
-storage.reload()
+type_storage = getenv('HBNB_TYPE_STORAGE')
+
+if type_storage == 'db':
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
