edda2858efaee23e6466359b7e5113efe8eaa882
PabloOsorix <3387@holbertonschool.com>
2022-05-15 12:39:47 -0500

    :heavy_plus_sign: add class DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module that define Class DBStorage which
+can help us to manipulate data in a database
+"""
 import os
 from os import getenv
 import sqlalchemy
@@ -7,23 +11,28 @@ from sqlalchemy.orm import sessionmaker
 import models
 from models.base_model import Base
 
-class DBStorage():
+
+class DBStorage:
     """
     Class that defines class to map and
     store in a database.
     """
-    
+
     __engine = None
     __session = None
+
     def __init__(self):
+        """Initialice enviroment variables and create
+        engine with database"""
+
         user = getenv('HBHB_MYSQL_USER')
         password = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
         database = getenv('HBNB_MYSSQL_DB')
         env = getenv('HBNB_ENV')
-        self.__engine = create_engine(host="{}", user="{}", passwd="{}", db="{}"
-                         .format(host, user, password, database ), 
-                          pool_pre_ping=True)
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/{}"
+                                      .format(host, user, password, database),
+                                      pool_pre_ping=True)
 
         if env == 'test':
             Base.drop_all(self.__engine)
@@ -41,14 +50,13 @@ class DBStorage():
         from models.amenity import Amenity
         from models.review import Review
 
-
         Session = sessionmaker(bind=self.__engine)
         self.__session = Session()
         dictionary = {}
 
         if cls is None:
             search_result = self.__session.query(User, Place, State, City,
-                                              Amenity, Review).all()
+                                                 Amenity, Review).all()
         else:
             search_result = self.__session.query(cls).all()
 
@@ -60,19 +68,25 @@ class DBStorage():
     def new(self, obj):
         """method that add currently object
         to the database"""
-        self.__session.add(obj)
-        self.save()
-        
+        if obj is not None:
+            try:
+                self.__session.add(obj)
+                self.__session.flush()
+                self.__session.refresh(obj)
+            except Exception as ex:
+                self.__session.rollback()
+                raise ex
+
     def save(self):
         """Method that add changes to the database"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Method that delete obj from the 
+        """Method that delete obj from the
         database current session"""
         if obj is not None:
-            self.__session.delete(obj)
-        self.save()
+            self.__session.query(type(obj)).filter(
+                type(obj).id == obj.id).delete()
 
     def reload(self):
         """Reload the currently database
@@ -90,4 +104,4 @@ class DBStorage():
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = scoped_session(session_factory)
+        self.__session = scoped_session(session_factory)()
