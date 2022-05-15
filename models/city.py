6963f325935cd60c170d17334f9225a404d99da6
PabloOsorix <3387@holbertonschool.com>
2022-05-15 12:26:09 -0500

    :heavy_plus_sign: add if else conditions depend type of the storage

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from  os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 
@@ -7,9 +8,10 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """Atributtes to create a class table city"""
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states_id'),
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states_id'),
                       nullable=False)
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    else:
+        name = ""
+        state_id = ""
