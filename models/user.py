a64b37515cbddeeead92a7073fd2fd73b424f257
PabloOsorix <3387@holbertonschool.com>
2022-05-15 18:59:59 -0500

    :heavy_minus_sign: delete import

--- models/user.py
+++ models/user.py
@@ -2,8 +2,7 @@
 """This module defines a class User"""
 from os import getenv
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
