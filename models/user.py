40e0a0eeb2a569ca4befbd5b5f6a4311841b6790
PabloOsorix <3387@holbertonschool.com>
2022-05-15 13:59:51 -0500

    :heavy_plus_sign: update user class

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    type_of_storage = getenv('HBNB_TYPE_STORAGE')
+    if type_of_storage == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
