5f82d270361ab8d6619e70ff17ea0ba881e9b526
PabloOsorix <3387@holbertonschool.com>
2022-05-15 14:11:07 -0500

    :heavy_plus_sign: add dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a class to manage database storage for hbnb clone"""
+"""This module defines a class to manage database
+storage for hbnb clone"""
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
