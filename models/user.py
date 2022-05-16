bbdc74b230b24663114e6983059a287faa657ce3
Diana M. Ayala <56034849+dmac24@users.noreply.github.com>
2022-05-16 16:49:27 -0500

    Update User - Style

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
