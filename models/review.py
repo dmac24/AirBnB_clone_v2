d8e490ff9ae8083e38b9ad595df3e4aff556655c
Diana M. Ayala <56034849+dmac24@users.noreply.github.com>
2022-05-16 17:47:10 -0500

    Update -  Delete relationship

--- models/review.py
+++ models/review.py
@@ -2,7 +2,6 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
@@ -11,7 +10,3 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-
-    user = relationship("User", backref="reviews")
-
-    place = relationship("Place", backref="reviews")
