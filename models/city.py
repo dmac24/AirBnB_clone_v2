f97070d8ce4d23574a5b6ccc256cffb2a55206a4
PabloOsorix <3387@holbertonschool.com>
2022-05-15 19:08:43 -0500

    :recycle: refact code

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import os
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -10,9 +10,10 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    type_of_storage = getenv('HBNB_TYPE_STORAGE')
+    if type_of_storage == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ''
+        state_id = ''
