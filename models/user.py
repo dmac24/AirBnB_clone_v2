420f34ff686dffca3e145b63cb734a5e88b793b3
PabloOsorix <3387@holbertonschool.com>
2022-05-16 15:53:38 -0500

    :heavy_plus_sign: add qualities to create data in db, and relationship places

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from os import getenv
+from tkinter import CASCADE
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -14,6 +16,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship('Place', cascade="all, delete, delete-orphan",
+                              backref='user')
     else:
         email = ''
         password = ''
