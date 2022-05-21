a8a3671074378617294214904a192ff37ff994b1
PabloOsorix <3387@holbertonschool.com>
2022-05-21 17:09:38 -0500

    :heavy_plus_sign: cript that generates a .tgz archive from the contents of the web_static
    folder of your AirBnB Clone repo, using the function do_pack.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz
+archive from the contents of the web_static
+folder of your AirBnB Clone repo,
+using the function do_pack.
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Fabric script that generates a .tgz archive from the contents of
+    the web_static folder of your AirBnB Clone repo,
+    using the function do_pack."""
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    tgz_file = "versions/web_static_{}.tgz".format(date)
+    tar_exec = local("tar -cvzf {} web_static".format(tgz_file))
+
+    if tar_exec.succeeded:
+        return tgz_file
+    else:
+        return None
